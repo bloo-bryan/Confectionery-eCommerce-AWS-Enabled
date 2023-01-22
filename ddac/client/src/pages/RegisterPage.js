469f4da77ee8fb9666546caaee20585b63526f50
JadenChun <69668411+JadenChun@users.noreply.github.com>
2023-01-22 13:34:15 +0800

    added css for register page

--- ddac/client/src/pages/RegisterPage.js
+++ ddac/client/src/pages/RegisterPage.js
@@ -1,6 +1,7 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
+import styled from 'styled-components'
 import { hideLoginPopUp, registerPost, checkUsernamePost } from '../features/userSlice';
 import {useEffect, useState} from 'react';
 import { useDispatch } from 'react-redux';
@@ -80,39 +81,45 @@ const RegisterPage = ()=>{
 
 
     return(
-        <>
-        <Form onSubmit={handleSubmit}>
-            <h1>Register A New Account</h1>
+        <Wrapper>
+            <div className='title-background'>
+                <div className='title-container'>
+                    <h1 className='title'>Register A New Account</h1>
+                </div>
+            </div>
+        <Form onSubmit={handleSubmit} className='form'>
             <Form.Group>
                 <Form.Label>Username</Form.Label>
                 <Form.Control type="username" placeholder="Enter username" onChange={ ()=>setUsernameValid(true) }/>
-                {usernameValid ? (<p></p>):(<p>This username has been taken, please try another username</p>)}
+                {usernameValid ? (<br/>):(<p className='warning'>This username has been taken, please try another username</p>)}
             </Form.Group>
             <Form.Group>
                 <Form.Label>Password</Form.Label>
                 <Form.Control type="password" placeholder="Enter password" />
             </Form.Group>
+            <br/>
             <Form.Group>
                 <Form.Label>Verify Password</Form.Label>
                 <Form.Control type="password" placeholder="Enter same password" onChange={handlePasswordRetry}/>
-                {passwordValid ? (<p></p>):(<p>The password is not same, please re-enter the password</p>)}
+                {passwordValid ? (<br/>):(<p className='warning'>The password is not same, please re-enter the password</p>)}
             </Form.Group>
             <Form.Group>
             <Form.Label>Registering as customer or merchant?</Form.Label>
-                <Form.Select aria-label="What role are you register for?" onChange={ (event)=>setRole(event.target.value) }>
+                <Form.Select className='dropdown' aria-label="What role are you register for?" onChange={ (event)=>setRole(event.target.value) }>
                     <option value='customer'>Customer</option>
                     <option value='merchant'>Merchant</option>
                 </Form.Select>
             </Form.Group>
+            <br/>
             {(role === 'customer')?
             (
                 <Form.Group>
-                    <Form.Label>Mobile number</Form.Label>
-                    <Form.Control type='phone-number' placeholder='Enter your mobile number' />
-                    <Form.Label>Shipping address</Form.Label>
-                    <Form.Control type='address' placeholder='Enter your shipping address' />
+                    <Form.Label>Mobile Number</Form.Label>
+                    <Form.Control type='phone-number' placeholder='Enter your mobile number' /> <br/>
+                    <Form.Label>Shipping Address</Form.Label>
+                    <Form.Control type='address' placeholder='Enter your shipping address' /> <br/>
                     <Form.Label>State</Form.Label>
-                    <Form.Select aria-label="state">
+                    <Form.Select aria-label="state" className='dropdown'>
                         <option value='kuala-lumpur'>Kuala Lumpur</option>
                         <option value='johor'>Johor</option>
                         <option value='kedah'>Kedah</option>
@@ -131,11 +138,11 @@ const RegisterPage = ()=>{
                 </Form.Group>
             ):(
                 <Form.Group>
-                    <Form.Label>Mobile number</Form.Label>
+                    <Form.Label>Mobile Number</Form.Label>
                     <Form.Control type='phone-number' placeholder='Enter your mobile number' />
                 </Form.Group>
             )}
-            <Button variant="primary" type="submit">
+            <Button variant="primary" type="submit" className='register-button'>
                 Register
             </Button>
         </Form>
@@ -147,8 +154,50 @@ const RegisterPage = ()=>{
             </Button>
             </Modal.Footer>
         </Modal>
-        </>
+        </Wrapper>
     )
 }
+
+const Wrapper = styled.div`
+.title-background{
+    background: var(--clr-primary-10);
+    width: 100%;
+    min-height: 20vh;
+    display: flex;
+    align-items: center;
+    margin-bottom: 5%;
+}
+.title-container{
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    width: 60%;
+}
+.title{
+    text-align: left;
+}
+.form{
+    margin-left: auto;
+    margin-right: auto;
+    width: 60%;
+}
+.warning{
+    color: red;
+}
+.register-button{
+    float: right;
+    margin-top 5%;
+    margin-bottom: 10vh;
+}
+button{
+    background: var(--clr-primary-5);
+    border-color: var(--clr-primary-5);
+}
+button:hover{
+    border-color: var(--clr-primary-5);
+    color: var(--clr-primary-5);
+    background: white;
+}
+`
 
 export default RegisterPage
